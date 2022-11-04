644333b13868ce12e2ee7fe484768d7563b0ff1c
Amine Ben Dahmen <aminedahmen14@gmail.com>
2022-11-04 01:28:47 +0100

    add unit-tests for produitService

--- src/main/java/tn/esprit/rh/achat/entities/Produit.java
+++ src/main/java/tn/esprit/rh/achat/entities/Produit.java
@@ -1,21 +1,15 @@
 package tn.esprit.rh.achat.entities;
 
-import java.io.Serializable;
-import java.util.Date;
-import java.util.Set;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -24,32 +18,37 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Produit implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idProduit;
-	private String codeProduit;
-	private String libelleProduit;
-	private float prix;
-	@Temporal(TemporalType.DATE)
-	private Date dateCreation;
-	@Temporal(TemporalType.DATE)
-	private Date dateDerniereModification;
-	@ManyToOne
-	@JsonIgnore
-	private Stock stock;
-	@OneToMany(mappedBy = "produit")
-	@JsonIgnore
-	private Set<DetailFacture> detailFacture;
-	@ManyToOne
-	@JsonIgnore
-	private CategorieProduit categorieProduit;
-	
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduit;
+    private String codeProduit;
+    private String libelleProduit;
+    private float prix;
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+    @Temporal(TemporalType.DATE)
+    private Date dateDerniereModification;
+    @ManyToOne
+    @JsonIgnore
+    private Stock stock;
+    @OneToMany(mappedBy = "produit")
+    @JsonIgnore
+    private Set<DetailFacture> detailFacture;
+    @ManyToOne
+    @JsonIgnore
+    private CategorieProduit categorieProduit;
 
 
-	
-
+    public Produit(Long idProduit, String codeProduit, String libelleProduit, float prix, Date dateCreation, Date dateDerniereModification) {
+        this.idProduit = idProduit;
+        this.codeProduit = codeProduit;
+        this.libelleProduit = libelleProduit;
+        this.prix = prix;
+        this.dateCreation = dateCreation;
+        this.dateDerniereModification = dateDerniereModification;
+    }
 }
---
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplTest.java
@@ -0,0 +1,72 @@
+package tn.esprit.rh.achat.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.repositories.ProduitRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class ProduitServiceImplTest {
+
+    @Mock
+    ProduitRepository produitRepository;
+    @InjectMocks
+    ProduitServiceImpl produitService;
+
+    List<Produit> list = new ArrayList<>(
+            Arrays.asList(
+                    new Produit(1L, "P1", "Product1", 10F, new Date(), new Date()),
+                    new Produit(2L, "P2", "Product2", 10F, new Date(), new Date())
+            )
+    );
+
+    Produit produit = new Produit(3L, "P3", "Product3", 10F, new Date(), new Date());
+
+    @Test
+    @DisplayName("Test should retrieve all products")
+    public void retrieveAllProduits() {
+        when(produitRepository.findAll()).thenReturn(list);
+        Assertions.assertEquals(2, produitService.retrieveAllProduits().size());
+    }
+
+    @Test
+    @DisplayName("Test should add a product")
+    public void addProduit() {
+        when(produitRepository.save(produit)).thenReturn(produit);
+        Assertions.assertEquals(produit, produitService.addProduit(produit));
+    }
+
+    @Test
+    @DisplayName("Test should delete a product")
+    public void deleteProduit() {
+    }
+
+    @Test
+    @DisplayName("Test should update a product")
+    public void updateProduit() {
+    }
+
+    @Test
+    @DisplayName("Test should retrieve a product")
+    public void retrieveProduit() {
+    }
+
+    @Test
+    @DisplayName("Test should assign a product to stock")
+    public void assignProduitToStock() {
+    }
+}
