2de3af10d467c82bcc6e9532a7129aa2c617a834
mohamedbenrabah <mohamed.benrabah98@gmail.com>
2022-11-17 19:40:39 +0100

    implement test secteur activite

--- src/main/java/tn/esprit/rh/achat/dto/SecteurActiviteDto.java
+++ src/main/java/tn/esprit/rh/achat/dto/SecteurActiviteDto.java
@@ -1,8 +1,10 @@
 package tn.esprit.rh.achat.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class SecteurActiviteDto {
     private Long idSecteurActivite;
     private String codeSecteurActivite;
---
+++ src/test/java/tn/esprit/rh/achat/services/SecteurActiviteServiceImplTest.java
@@ -0,0 +1,106 @@
+package tn.esprit.rh.achat.services;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.rh.achat.entities.SecteurActivite;
+import tn.esprit.rh.achat.repositories.SecteurActiviteRepository;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(OrderAnnotation.class)
+class SecteurActiviteServiceImplTest {
+	
+	@InjectMocks
+	SecteurActiviteServiceImpl service;
+	
+	@Mock
+	SecteurActiviteRepository repository;
+
+	SecteurActivite secteurActivite = SecteurActivite.builder()
+			.idSecteurActivite(1L)
+			.codeSecteurActivite("8Z4DC")
+			.libelleSecteurActivite("secteur informatique")
+			.build();
+	
+	@Test
+	@Order(1)
+	void testRetrieveAllSecteurActivite() {
+		SecteurActivite secteurActivite1 = SecteurActivite.builder()
+				.idSecteurActivite(2L)
+				.codeSecteurActivite("AZ123E")
+				.libelleSecteurActivite("secteur marketing")
+				.build();
+		
+		//si la liste est remplie
+		when(repository.findAll()).thenReturn(new ArrayList<SecteurActivite>(Arrays.asList(secteurActivite, secteurActivite1)));
+		List<SecteurActivite> listSecteurActivites = service.retrieveAllSecteurActivite();
+		assertEquals(2, listSecteurActivites.size());
+		
+		//si la liste est vide
+		when(repository.findAll()).thenReturn(Collections.emptyList());
+		listSecteurActivites = service.retrieveAllSecteurActivite();
+		assertEquals(true, listSecteurActivites.isEmpty());	
+	}
+	
+	@Test
+	@Order(2)
+	void testAddSecteurActivite() {
+		when(repository.save(secteurActivite)).thenReturn(secteurActivite);
+		
+		SecteurActivite savedActivite = service.addSecteurActivite(secteurActivite);
+	
+		assertEquals(secteurActivite, savedActivite);
+		assertNotNull(savedActivite);
+	}
+	
+	@Test
+	@Order(3)
+	void testDeleteSecteurActivite() {
+		long idSecteurActivite = 1l;
+		service.deleteSecteurActivite(idSecteurActivite);
+		verify(repository, times(1)).deleteById(secteurActivite.getIdSecteurActivite());
+	}
+	
+	@Test
+	@Order(4)
+	void testUpdateSecteurActivite() {
+		when(repository.save(secteurActivite)).thenReturn(secteurActivite);
+		secteurActivite.setCodeSecteurActivite("X25ED3");
+		secteurActivite.setLibelleSecteurActivite("secteur industriel");
+		
+		SecteurActivite updatedSecteurActivite = service.updateSecteurActivite(secteurActivite);
+		assertEquals("X25ED3", updatedSecteurActivite.getCodeSecteurActivite());
+		assertEquals("secteur industriel", updatedSecteurActivite.getLibelleSecteurActivite());
+	}
+	
+	@Test
+	@Order(5)
+	void testRetrieveSecteurActivite() {
+		long idSecteurActivite = 1l;
+		when(repository.findById(idSecteurActivite)).thenReturn(Optional.of(secteurActivite));
+		
+		SecteurActivite searchedSecteurActivite = service.retrieveSecteurActivite(idSecteurActivite);
+		assertNotNull(searchedSecteurActivite);
+	}
+}
